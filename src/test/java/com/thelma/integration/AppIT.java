815f810ddbe435190dee6f623557ff2c0131624b
thelmagali <thelmabalbuena@gmail.com>
2020-02-16 18:25:03 -0300

    Added more integration tests.

--- src/test/java/com/thelma/integration/AppIT.java
+++ src/test/java/com/thelma/integration/AppIT.java
@@ -1,4 +1,4 @@
-package com.thelma.controller;
+package com.thelma.integration;
 
 import com.thelma.App;
 import org.junit.jupiter.api.Assertions;
@@ -10,12 +10,11 @@ import java.util.Scanner;
 
 class AppIT {
 
-    void testBowlingGame(String inputFile) throws IOException {
+    void testHelper(String inputFile, String inputDir) throws IOException {
         final String EXP_OUT_DIR = "src/test/resources/expected-output/";
-        final String INPUT_DIR = "src/test/resources/";
         File outputFile = File.createTempFile(inputFile, "tmp");
         System.setOut(new PrintStream(new FileOutputStream(outputFile)));
-        App.main(new String[]{INPUT_DIR + inputFile});
+        App.main(new String[]{inputDir + inputFile});
         try (Scanner expectedScanner = new Scanner(new File(EXP_OUT_DIR + inputFile));
              Scanner actualScanner = new Scanner(outputFile)) {
             while (expectedScanner.hasNext()) {
@@ -24,20 +23,35 @@ class AppIT {
         }
     }
 
+    void testHelper(String inputFile) throws IOException {
+        final String INPUT_DIR = "src/test/resources/";
+        testHelper(inputFile, INPUT_DIR);
+    }
+
+    @Test
+    void testSampleInput() throws IOException {
+        testHelper("sample-input", "src/main/resources/");
+    }
+
 
     @Test
     void testPerfectGame() throws IOException {
-       testBowlingGame("perfect-game-single");
+       testHelper("perfect-game-single");
     }
 
     @Test
     void testIncompleteGame() throws IOException {
-        testBowlingGame("incomplete-game");
+        testHelper("incomplete-game");
     }
 
     @Test
     void testRandomGame1() throws IOException {
-        testBowlingGame("2-players-random");
+        testHelper("2-players-random");
+    }
+
+    @Test
+    void testZeroGame() throws IOException {
+        testHelper("zero-game");
     }
 
 
