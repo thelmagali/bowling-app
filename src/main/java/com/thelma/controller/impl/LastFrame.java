0e90f10617929179f9399ca15af039dc72dbe72b
thelmagali <thelmabalbuena@gmail.com>
2020-02-16 10:38:25 -0300

    reverted LastFrame.

--- src/main/java/com/thelma/controller/impl/LastFrame.java
+++ src/main/java/com/thelma/controller/impl/LastFrame.java
@@ -2,38 +2,16 @@ package com.thelma.controller.impl;
 
 import com.thelma.controller.Frame;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LastFrame extends Frame {
 
-    private List<Character> chances = new ArrayList<>();
-
     @Override
-    protected Iterable<Character> getChances() {
-        return chances;
-    }
-
-    @Override
-    protected Character getChance(int index) {
-        return chances.get(index);
-    }
-
-    @Override
-    protected void setChance(int index, Character chance) throws Exception {
-        if(index == 2){
-            if(isComplete()){
-                throw new Exception("Invalid throw. Frame is complete");
-            }
-            chances.add(chance);
-        } else{
-            chances.set(index, chance);
-        }
+    protected int getNumberOfChances() {
+        return 3;
     }
 
     @Override
     public boolean isComplete() {
-        return (ballsToScore == 0) ? (chances.size() == 2) : (chances.size() == 3);
+        return (ballsToScore == 0) ? (currentIdx == 1) : (currentIdx == 2);
     }
 
     @Override
