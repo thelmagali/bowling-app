a227c06104d30f2bb61672b6890a6e2f5db1b379
thelmagali <thelmabalbuena@gmail.com>
2020-02-16 11:08:53 -0300

    Added plugins for the integration test.

---
+++ src/test/java/com/thelma/integration/AppIT.java
@@ -0,0 +1,13 @@
+package com.thelma.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppIT {
+    @Test
+    public void simpleIntegrationTestIT() {
+        System.out.println("integration test");
+        assertTrue(false);
+    }
+}
