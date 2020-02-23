4293f86d63ab3d3694de6cd25c55be6c68cae675
thelmagali <thelmabalbuena@gmail.com>
2020-02-23 09:22:17 -0300

    Added more tests.

--- src/test/java/com/thelma/controller/GameInputReaderFileTest.java
+++ src/test/java/com/thelma/controller/GameInputReaderFileTest.java
@@ -11,9 +11,29 @@ class GameInputReaderFileTest {
 
     @Test
     @DisplayName("Show throw FileNotFoundException when trying to create a reader with a non existing file")
-    void shouldThrowError() {
+    void shouldThrowError1() {
         Assertions.assertThrows(FileNotFoundException.class, () ->
             new GameInputReaderFile("NON_EXISTING_FILENAME")
         );
+    }
+
+    @Test
+    @DisplayName("Throw error when line does not have a tab")
+    void shouldThrowError2() {
+        Exception thrown = Assertions.assertThrows(Exception.class, () ->{
+            GameInputReader reader = new GameInputReaderFile("invalid-input-1");
+            reader.readChance();
+        });
+        Assertions.assertEquals("Line must have 2 elements", thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Throw error when pinfalls has an invalid value")
+    void shouldThrowError3(){
+        Exception thrown = Assertions.assertThrows(Exception.class, () ->{
+            GameInputReader reader = new GameInputReaderFile("invalid-input-2");
+            reader.readChance();
+        });
+        Assertions.assertEquals("Invalid value of pinfalls", thrown.getMessage());
     }
 }
